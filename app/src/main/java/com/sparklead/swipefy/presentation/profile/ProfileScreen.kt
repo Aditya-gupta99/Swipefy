6b2fa00e42022097848558413258de69742f7937
Pratyush Singh <aries.pratyush@gmail.com>
2023-12-06 12:02:01 +0530

    chore: profile screen design & required deps

--- app/src/main/java/com/sparklead/swipefy/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/sparklead/swipefy/presentation/profile/ProfileScreen.kt
@@ -2,26 +2,48 @@
 
 package com.sparklead.swipefy.presentation.profile
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberImagePainter
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.sparklead.swipefy.R
 import com.sparklead.swipefy.presentation.theme.Black
 
 @Composable
@@ -29,6 +51,19 @@ fun ProfileScreen(navController: NavController) {
 
     val systemUiController = rememberSystemUiController()
     val statusBarColor = Color(0xFF1DB954)
+
+    val imageUri = rememberSaveable { mutableStateOf("") }
+    val painter = rememberImagePainter(
+        if (imageUri.value.isEmpty())
+            R.drawable.ic_person
+        else
+            imageUri.value
+    )
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        uri?.let { imageUri.value = it.toString() }
+    }
 
     DisposableEffect(key1 = true) {
         systemUiController.setStatusBarColor(statusBarColor)
@@ -40,29 +75,168 @@ fun ProfileScreen(navController: NavController) {
         }
     }
 
-    Scaffold(
+    Column(
         modifier = Modifier
-            .fillMaxSize(),
-        containerColor = Black
-    ) { padding ->
-        Column(modifier = Modifier.padding(padding)) {
-            Row(
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier
-                    .height(255.dp)
-                    .fillMaxWidth()
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(
-                                Color(0xFF1DB954),
-                                Color.Black.copy(0.75f)
-                            )
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .height(255.dp)
+                .fillMaxWidth()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color(0xFF1DB954),
+                            Color.Black.copy(0.75f)
                         )
                     )
+                )
+        ) {
+            Card(
+                shape = CircleShape,
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .size(130.dp)
             ) {
+                Image(
+                    painter = painter,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .background(Color.Black)
+                        .clickable { launcher.launch("image/*") },
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = "Ankur Singh",
+                style = TextStyle(
+                    fontSize = 28.sp,
+                    fontFamily = FontFamily(Font(R.font.outfit_medium))
+                ),
+                color = Color.White,
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.swipe_stats),
+                color = Color.White,
+                style = TextStyle(
+                    fontSize = 21.sp,
+                    fontFamily = FontFamily(Font(R.font.outfit_medium))
+                )
+            )
+        }
+
+        Spacer(Modifier.height(8.dp))
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+            Column {
+                Text(
+                    text = "26",
+                    color = Color.White,
+                    style = TextStyle(
+                        fontSize = 21.sp,
+                        fontFamily = FontFamily(Font(R.font.outfit_medium))
+                    )
+                )
+                Text(
+                    text = stringResource(R.string.swipes),
+                    color = Color.White,
+                    style = TextStyle(
+                        fontSize = 21.sp,
+                        fontFamily = FontFamily(Font(R.font.outfit_medium))
+                    )
+                )
 
             }
+            Column {
+                Text(
+                    text = "0",
+                    color = Color.White,
+                    style = TextStyle(
+                        fontSize = 21.sp,
+                        fontFamily = FontFamily(Font(R.font.outfit_medium))
+                    )
+                )
+                Text(
+                    text = stringResource(R.string.liked),
+                    color = Color.White,
+                    style = TextStyle(
+                        fontSize = 21.sp,
+                        fontFamily = FontFamily(Font(R.font.outfit_medium))
+                    )
+                )
+
+            }
+            Column {
+                Text(
+                    text = "0",
+                    color = Color.White,
+                    style = TextStyle(
+                        fontSize = 21.sp,
+                        fontFamily = FontFamily(Font(R.font.outfit_medium))
+                    )
+                )
+                Text(
+                    text = stringResource(R.string.disliked),
+                    color = Color.White,
+                    style = TextStyle(
+                        fontSize = 21.sp,
+                        fontFamily = FontFamily(Font(R.font.outfit_medium))
+                    )
+                )
+
+            }
+
+            Column {
+                Text(
+                    text = "0",
+                    color = Color.White,
+                    style = TextStyle(
+                        fontSize = 21.sp,
+                        fontFamily = FontFamily(Font(R.font.outfit_medium))
+                    )
+                )
+                Text(
+                    text = stringResource(R.string.skips),
+                    color = Color.White,
+                    style = TextStyle(
+                        fontSize = 21.sp,
+                        fontFamily = FontFamily(Font(R.font.outfit_medium))
+                    )
+                )
+
+            }
+
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp)) {
+            Text(
+                text = stringResource(R.string.swipe_activity),
+                color = Color.White,
+                style = TextStyle(
+                    fontSize = 21.sp,
+                    fontFamily = FontFamily(Font(R.font.outfit_medium))
+                )
+            )
+        }
+
     }
 }
 
