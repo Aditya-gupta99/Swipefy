f25f4a289488c3586d43373eea62d860711d265d
Aditya Gupta <ashishgupta9936ad@gmail.com>
2024-01-06 19:15:38 +0530

    app: Ktor Client setup

--- app/src/main/java/com/sparklead/swipefy/di/ApplicationModule.kt
+++ app/src/main/java/com/sparklead/swipefy/di/ApplicationModule.kt
@@ -5,6 +5,15 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.features.HttpTimeout
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logging
+import io.ktor.client.features.websocket.WebSockets
+import kotlinx.serialization.json.Json
 import javax.inject.Singleton
 
 @Module
@@ -15,4 +24,23 @@ object ApplicationModule {
     @Singleton
     fun providesFirebaseAuth() = FirebaseAuth.getInstance()
 
+    @Provides
+    @Singleton
+    fun provideHttpClient(): HttpClient {
+        return HttpClient(Android) {
+            install(WebSockets)
+            install(JsonFeature) {
+                serializer = KotlinxSerializer(Json {
+                    ignoreUnknownKeys = true
+                    isLenient = true
+                })
+            }
+            install(HttpTimeout) {
+                requestTimeoutMillis = 15000
+            }
+            install(Logging) {
+                level = LogLevel.ALL
+            }
+        }
+    }
 }
